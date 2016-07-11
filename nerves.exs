3bf44384ca8ef220e062f74e3a98e92bec77e83f
Marcin Operacz <marcin.operacz@gmail.com>
2016-07-11 16:56:43 -0400

    typo

--- nerves.exs
+++ nerves.exs
@@ -10,6 +10,7 @@ config :nerves_system_rpi3, :nerves_env,
   version: version,
   mirrors: [
     "https://github.com/evokly/nerves_system_rpi3/releases/download/v#{version}/nerves_system_rpi3.tar.gz",
+  ],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
