bba2fa5c2cb344d4ff47d1cc2cfee259e5075d64
Marcin Operacz <marcin.operacz@gmail.com>
2016-07-11 16:54:29 -0400

    change url to system

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,7 @@ config :nerves_system_rpi3, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/evokly/nerves_system_rpi3/releases/download/v#{version}/nerves_system_rpi3-v#{version}.tar.gz",
+    "https://github.com/evokly/nerves_system_rpi3/releases/download/v#{version}/nerves_system_rpi3.tar.gz",
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
